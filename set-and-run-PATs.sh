7704e70d6064318297b04261244700b04d2a6edf
Jesse Alford <jalford@pivotallabs.com>
2014-07-10 21:01:39 -0700

    modify set-and-run script to use admin user and set quota

--- set-and-run-PATs.sh
+++ set-and-run-PATs.sh
@@ -2,11 +2,8 @@
 ## **NB:** This script requires the `mktemp` tool.
 ## It uses only portable features of the tool, so it should work with any implementation of it that might be present.
 ##
-## It also requires the binary version of the PAT tool, available at https://github.com/cloudfoundry-incubator/pat
-##
-## Finally, this script requires that a PAT-org be setup with adequite quota on the target CF, 
-## and that $PATUSER and $PATPSWD are set to a user with permissions to push to that space.
-## (The admin user is fine, but not required, for this.)
+## It also requires the binary version of the PAT tool (`go build` will generate this binary in this repository)
+## $PATUSER and $PATPSWD must be set to admin for the targeted environment.
 
 # Exit on error
 set -e
@@ -15,23 +12,25 @@ set -e
 CF_API=$1
 CONFIG_FILE=$2
 
-# set random, temporary CF_HOME with an absolute path
+# create and set random, temporary CF_HOME with an absolute path
 
-export CF_HOME="${PWD}/"
-export CF_HOME="${CF_HOME}$(mktemp -d random-cf-home.XXXXX)"
+export CF_HOME="${PWD}/$(mktemp -d random-cf-home.XXXXX)"
 
 # login to CF, create and target PAT-space
 cf api $CF_API --skip-ssl-validation
 cf auth $PATUSER $PATPSWD
+cf create-org PAT-org
 cf create-space PAT-space -o PAT-org
 cf target -o PAT-org -s PAT-space
+cf set-quota PAT-org runaway
 
 # run PAT
 ./pat -config="${CONFIG_FILE}" 
 
 # move test results to a human-readable name
 # and put it in ./output to avoid problems w/non-PAT filenames in ./output/csvs
-mv output/csvs/*.csv "output/${CF_API}.csv"
+## DISABLED - probably have to manage this manually until/unless I can figure out a slicker system
+# mv output/csvs/*.csv "output/${CF_API}.csv"
 
 # clean up test artifacts
 cf delete-space -f PAT-space
